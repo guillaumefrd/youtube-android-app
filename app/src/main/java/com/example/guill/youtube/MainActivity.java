d7f415c5e928f971438cc0fdf67916823b657b1b
guillaumefrd <guillaume-fradet@hotmail.fr>
2018-03-10 20:48:01 +0100

    (feature): keep history of all the queries + modified the search interface with a searchview and autocomplete textbox

--- app/src/main/java/com/example/guill/youtube/MainActivity.java
+++ app/src/main/java/com/example/guill/youtube/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.guill.youtube;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.KeyEvent;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
@@ -19,7 +30,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements View.OnKeyListener {
+public class MainActivity extends AppCompatActivity {
 
     private String key = "AIzaSyCU-Al-NuszNKX9QRiDiJ9E7BI_7Aa-MXk";
     private String part = "snippet";
@@ -28,18 +39,25 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
     private VideoRecyclerAdapter adapter;
     private List<Item> videos;
 
+    private static final String PREFS = "PREFS";
+    private static final String PREFS_SEARCH = "PREFS_SEARCH";
+    SharedPreferences sharedPreferences;
+
+    private Context parent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText searchBox = findViewById(R.id.searchBox);
-        searchBox.setOnKeyListener(this);
+        sharedPreferences = getBaseContext().getSharedPreferences(PREFS, MODE_PRIVATE);
 
         videos = new ArrayList<Item>();
         initializeRecyclerView();
         initializeAdapter(videos);
-        youtubeAPICall();
+        youtubeAPICall("");
+
+        parent = this;
 
         recyclerView.addOnItemTouchListener(
                 new RecyclerItemClickListener(this, recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
@@ -52,6 +70,113 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
         );
     }
 
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        MenuItem menu_search = menu.findItem(R.id.menu_search);
+        MenuItem menu_home = menu.findItem(R.id.menu_home);
+
+        menu_home.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                youtubeAPICall("");
+                return false;
+            }
+        });
+
+        menu_search.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                item.getActionView().requestFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+                return true;
+            }
+        });
+
+        SearchView searchView = (SearchView) menu_search.getActionView();
+
+        final SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchAutoComplete.setDropDownBackgroundResource(android.R.color.white);
+        searchAutoComplete.setThreshold(0);
+
+        // make searchview results occupy whole screen
+        final AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) searchAutoComplete.findViewById(R.id.search_src_text);
+        final View dropDownAnchor = searchView.findViewById(autoCompleteTextView.getDropDownAnchor());
+        if (dropDownAnchor != null) {
+            dropDownAnchor.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+                @Override
+                public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                    Rect screenSize = new Rect();
+                    getWindowManager().getDefaultDisplay().getRectSize(screenSize);
+                    autoCompleteTextView.setDropDownWidth(screenSize.width());
+                }
+            });
+        }
+
+        if(sharedPreferences.contains(PREFS_SEARCH)) {
+            final String dataArr[] = sharedPreferences.getString(PREFS_SEARCH, null).split(";");
+            ArrayAdapter<String> autocompleteAdapter = new ArrayAdapter<String>(this, R.layout.dropdown, dataArr);
+            searchAutoComplete.setAdapter(autocompleteAdapter);
+        }
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                youtubeAPICall(query);
+
+                if(sharedPreferences.contains(PREFS_SEARCH)) {
+                    // check if this query already exist
+                    if(!Arrays.asList(sharedPreferences.getString(PREFS_SEARCH, null).split(";")).contains(query)){
+                        sharedPreferences
+                                .edit()
+                                .putString(PREFS_SEARCH, sharedPreferences.getString(PREFS_SEARCH, null) + ";" + query)
+                                .apply();
+                    }
+                } else {
+                    sharedPreferences
+                            .edit()
+                            .putString(PREFS_SEARCH, query)
+                            .apply();
+                }
+
+                // update autocomplete
+                String newDataArr[] = sharedPreferences.getString(PREFS_SEARCH, null).split(";");
+                ArrayAdapter<String> newAutocompleteAdapter = new ArrayAdapter<String>(parent, R.layout.dropdown, newDataArr);
+                searchAutoComplete.setAdapter(newAutocompleteAdapter);
+
+                // hide keybord
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(findViewById(R.id.recyclerView).getWindowToken(), 0);
+
+                return true;
+            }
+        });
+
+        searchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int itemIndex, long id) {
+                String queryString=(String)adapterView.getItemAtPosition(itemIndex);
+                searchAutoComplete.setText("" + queryString);
+            }
+        });
+
+        return true;
+    }
+
+
     private void initializeRecyclerView() {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -62,15 +187,14 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
         recyclerView.setAdapter(adapter);
     }
 
-    private void youtubeAPICall() {
-        EditText searchBox = findViewById(R.id.searchBox);
+    private void youtubeAPICall(String query) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://www.googleapis.com/youtube/v3/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         YoutubeAPI youtubeAPI = retrofit.create(YoutubeAPI.class);
-        Call<SOAnswersResponse> call = youtubeAPI.getAnswers(key, part, searchBox.getText().toString(), "video", 10);
+        Call<SOAnswersResponse> call = youtubeAPI.getAnswers(key, part, query, "video", 10);
         call.enqueue(new Callback<SOAnswersResponse>() {
             @Override
             public void onResponse(Call<SOAnswersResponse> call, Response<SOAnswersResponse> response) {
@@ -82,26 +206,9 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
 
             @Override
             public void onFailure(Call<SOAnswersResponse> call, Throwable t) {
-
+                Toast.makeText(parent, "Network error: failed to load the videos.", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    @Override
-    public boolean onKey(View view, int keyCode, KeyEvent event) {
-        if (keyCode == EditorInfo.IME_ACTION_SEARCH ||
-                keyCode == EditorInfo.IME_ACTION_DONE ||
-                event.getAction() == KeyEvent.ACTION_DOWN &&
-                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-
-            if (!event.isShiftPressed()) {
-
-                youtubeAPICall();
-                return true;
-            }
-
-        }
-        return false;
-
-    }
 }
